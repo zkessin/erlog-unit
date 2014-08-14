7f609c1214db624e771e7c5a8748e56256bc84d8
Zachary Kessin <zkessin@gmail.com>
2014-08-14 20:35:40 +0300

    setup test framework

---
+++ src/erlog_unit.erl
@@ -0,0 +1,7 @@
+-module(erlog_unit).
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+
+
+
+
---
+++ test/erlog_unit_tests.erl
@@ -0,0 +1,36 @@
+-module(erlog_unit_tests).
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+
+
+load_file_and_assertions_test() ->
+    nyi.
+
+load_find_tests_test() ->
+    nyi.
+
+spawn_and_execute_tests_test() ->
+    nyi.
+
+assert_true_test() ->
+    nyi.
+
+assert_not_true_test() ->
+    nyi.
+
+assert_equals_test() ->
+    nyi.
+
+assert_not_equals_test() ->
+    nyi.
+
+assert_foreach_test() ->
+    nyi.
+
+assert_generate_int_test() ->
+    nyi.
+
+assert_shrink_test() ->
+    nyi.
+
+
